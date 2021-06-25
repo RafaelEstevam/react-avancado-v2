1bbfe7f5aa5f9de6d7c86212569687b55e589bf7
rafaelEstevam <rafael.estevamdesign@gmail.com>
2021-06-25 14:25:12 -0300

    configure jest

---
+++ react-avancado/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: 'jsdom', //simular o dom do browser
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'], //ignorar pastas para testes
+  collectCoverage: true, //habilitar o teste de cobertura
+  collectCoverageFrom: ['src/**/*.ts(x)'], //faz o teste em todos os arquivos dessa pasta
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+}
