7bc6ca4e6863c3b264541946d5d88f49f28210f8
rafaelEstevam <rafael.estevamdesign@gmail.com>
2021-07-28 12:48:23 -0300

    pwa config

--- react-avancado/jest.config.js
+++ react-avancado/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   testEnvironment: 'jsdom', //simular o dom do browser
   testPathIgnorePatterns: ['/node_modules/', '/.next/'], //ignorar pastas para testes
   collectCoverage: true, //habilitar o teste de cobertura
-  collectCoverageFrom: ['src/**/*.ts(x)'], //faz o teste em todos os arquivos dessa pasta
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'], //faz o teste em todos os arquivos dessa pasta
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'], //arquivo de setup
   moduleNameMapper: {
     'styled-componentes':
