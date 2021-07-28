506024563181980edfc0ee1caae9c3feec4371be
rafaelEstevam <rafael.estevamdesign@gmail.com>
2021-07-28 16:08:22 -0300

    plop generate

---
+++ src/components/Input/test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react'
+
+import Input from './'
+
+describe('<Input />', () => {
+  it('should render the heading', () => {
+    const { container } = render(<Input />)
+    expect(
+      screen.getByRole('heading', { name: /Input/i })
+    ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Input />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
+  })
+})
