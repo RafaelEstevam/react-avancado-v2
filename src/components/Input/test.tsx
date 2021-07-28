6857933f79500f3d78a320f11132ba1a05c0bc44
rafaelEstevam <rafael.estevamdesign@gmail.com>
2021-07-28 16:16:02 -0300

    plop generate

--- src/components/Input/test.tsx
+++ src/components/Input/test.tsx
@@ -5,9 +5,7 @@ import Input from './'
 describe('<Input />', () => {
   it('should render the heading', () => {
     const { container } = render(<Input />)
-    expect(
-      screen.getByRole('heading', { name: /Input/i })
-    ).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /Input/i })).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
   })
